61bcb41dadc5476c0ddb84a64ae99e2d8f80fce6
turinglabsorg <sebastiano.cataudo@gmail.com>
2022-03-22 19:08:13 +0100

    better readme

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-# magiclink-eth
+# VueJS / MagicLink integration
+
+![](https://media0.giphy.com/media/9r75ILTJtiDACKOKoY/giphy.gif?cid=ecf05e470vlnfa97n8igtg1fhs45te0hfr9g0rkdf6tzuqce&rid=giphy.gif&ct=g)
+
+This template wants to test out the functionality of Magic Link (https://magic.link/), used with Ethereum (Rinkeby).
+Any other EVM network is compatible (like Polygon) and you can use it to easily onboard your users in your app with their e-mail.
+Approach is not 100% "crypto" but there are use cases where this strategy can be interesting.
+
+Magic happens at `components/MagicLink.vue` where you can see you'll need a properly configured `.env` file like this:
+
+```
+VUE_APP_MAGIC_PUB_KEY=
+VUE_APP_CONTRACT_ADDRESS=
+VUE_APP_NETWORK=
+```
+
+After everything is setted up you can run VueJS project.
 
 ## Project setup
 ```
@@ -14,11 +30,3 @@ yarn serve
 ```
 yarn build
 ```
-
-### Lints and fixes files
-```
-yarn lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/components/MagicLink.vue
+++ src/components/MagicLink.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <div class="container">
-      <h1>Vue / MagicLink</h1>
+      <h1 style="margin-bottom: 30px">Vue / MagicLink</h1>
       <hr />
       <div v-if="!checking">
         <div v-if="!isLoggedIn">
@@ -22,17 +22,30 @@
           <b>Network is:</b><br />
           {{ network.name }}
           <br /><br />
-          <div v-if="isSending">Sending transaction, please wait..</div>
+          <hr />
+          <br />
+          You're minting
+          <a
+            href="https://testnets.opensea.io/collection/nonft-v2"
+            target="_blank"
+            >"nothing"</a
+          ><br />for 0.01 ETH at
+          <b>0xf81046bafce92bdac543f2a91fa91c16ba40abde</b><br /><br />
+          <div v-if="isSending" style="margin-bottom:30px">Sending transaction, please wait..</div>
           <button
+            style="margin-bottom: 30px"
             v-if="!tx && !isSending"
-            v-bind:disabled="userBalance <= 0.1"
+            v-bind:disabled="userBalance <= 0.01"
             v-on:click="mint"
           >
-            MINT</button
-          ><br /><br />
+            MINT
+          </button>
           <div v-if="tx">
             <b>Tx submitted at:</b><br />
-            <a :href="'https://rinkeby.etherscan.io/tx/' + tx.hash">{{ tx.hash }}</a><br /><br />
+            <a :href="'https://rinkeby.etherscan.io/tx/' + tx.hash">{{
+              tx.hash
+            }}</a
+            ><br /><br />
             <div v-if="receipt"><b>Receipt is:</b><br />{{ receipt }}</div>
           </div>
           <hr />
@@ -49,14 +62,11 @@
 import { Magic } from "magic-sdk";
 import { ethers } from "ethers";
 
-const magic = new Magic("pk_live_2B9B8268A7D5D33F", {
-  network: "rinkeby",
-});
-
 export default {
   name: "MagicLink",
   data() {
     return {
+      magic: {},
       email: "",
       userMetadata: {},
       userAddress: "",
@@ -65,7 +75,8 @@ export default {
       isLoggedIn: false,
       isSending: false,
       network: {},
-      contract_address: "0x9b3ae7419809ce1a690c8c5fc64d51949365dd75",
+      // This must be changed
+      contract_address: process.env.VUE_APP_CONTRACT_ADDRESS,
       ABI: [
         {
           inputs: [],
@@ -82,25 +93,28 @@ export default {
   },
   mounted() {
     const app = this;
+    app.magic = new Magic(process.env.VUE_APP_MAGIC_PUB_KEY, {
+      network: process.env.VUE_APP_NETWORK,
+    });
     app.render();
   },
   methods: {
     async loginWithEmail() {
       const app = this;
       if (app.email.length > 0) {
-        await magic.auth.loginWithMagicLink({ email: app.email });
+        await app.magic.auth.loginWithMagicLink({ email: app.email });
         app.render();
       }
     },
     async render() {
       const app = this;
-      const isLoggedIn = await magic.user.isLoggedIn();
+      const isLoggedIn = await app.magic.user.isLoggedIn();
       console.log("User is logged?", isLoggedIn);
       if (isLoggedIn) {
         app.isLoggedIn = true;
-        const provider = new ethers.providers.Web3Provider(magic.rpcProvider);
+        const provider = new ethers.providers.Web3Provider(app.magic.rpcProvider);
         console.log("User is logged in..");
-        app.userMetadata = await magic.user.getMetadata();
+        app.userMetadata = await app.magic.user.getMetadata();
         const signer = provider.getSigner();
         app.network = await provider.getNetwork();
         console.log("Network is: ", app.network);
@@ -117,14 +131,14 @@ export default {
     },
     async mint() {
       const app = this;
-      const isLoggedIn = await magic.user.isLoggedIn();
+      const isLoggedIn = await app.magic.user.isLoggedIn();
       console.log("User is logged?", isLoggedIn);
       if (isLoggedIn) {
         app.isSending = true;
-        const provider = new ethers.providers.Web3Provider(magic.rpcProvider);
+        const provider = new ethers.providers.Web3Provider(app.magic.rpcProvider);
         console.log("User is logged in..");
         try {
-          app.userMetadata = await magic.user.getMetadata();
+          app.userMetadata = await app.magic.user.getMetadata();
           const signer = provider.getSigner();
           let contract = new ethers.Contract(
             app.contract_address,
@@ -132,7 +146,7 @@ export default {
             signer
           );
           app.tx = await contract.buyNFT({
-            value: ethers.utils.parseEther("0.1"),
+            value: ethers.utils.parseEther("0.01"),
           });
           console.log("Submitted:", app.tx);
           app.receipt = await app.tx.wait();
@@ -146,7 +160,7 @@ export default {
     },
     async logout() {
       const app = this;
-      await magic.user.logout();
+      await app.magic.user.logout();
       app.isLoggedIn = false;
     },
   },
